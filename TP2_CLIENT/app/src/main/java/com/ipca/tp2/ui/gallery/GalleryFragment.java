6b8065a4dbee35b02a720453431e74e1ef4216f8
rui_rodrigues_10212 <a10212@alunos.ipca.pt>
2020-01-28 21:26:10 +0000

    maps integration

--- TP2_CLIENT/app/src/main/java/com/ipca/tp2/ui/gallery/GalleryFragment.java
+++ TP2_CLIENT/app/src/main/java/com/ipca/tp2/ui/gallery/GalleryFragment.java
@@ -1,5 +1,9 @@
 package com.ipca.tp2.ui.gallery;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,16 +12,29 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import com.ipca.tp2.R;
 
 public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
-
+    MapView mMapView;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
@@ -30,6 +47,62 @@ public class GalleryFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.frg);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+                mMap.clear(); //clear old markers
+
+                CameraPosition googlePlex = CameraPosition.builder()
+                        .target(new LatLng(37.4219999,-122.0862462))
+                        .zoom(100)
+                        .bearing(0)
+                        .tilt(45)
+                        .build();
+
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null);
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(37.4219999, -122.0862462))
+                        .title("Spider Man")
+                        .icon(bitmapDescriptorFromVector(getActivity(),R.drawable.spider)));
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(37.4629101,-122.2449094))
+                        .title("Iron Man")
+                        .snippet("His Talent : Plenty of money"));
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(37.3092293,-122.1136845))
+                        .title("Captain America"));
+            }
+        });
+
+
+
+
+
         return root;
+    }
+
+
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
 }
